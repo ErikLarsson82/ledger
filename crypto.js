146592efc0a8037efc9bf9512a25289975378979
Erik Larsson <erik.mattias.larsson@gmail.com>
2017-08-26 11:37:02 +0200

    Hello world encryption

---
+++ crypto.js
@@ -0,0 +1,20 @@
+/*
+krypto spec
+
+a <> b <> c
+
+private a > b > c
+
+public a < b < c
+*/
+
+const NodeRSA = require('node-rsa')
+const pubKey = new NodeRSA({ b: 512 })
+
+const text = 'Hello RSA!'
+const encrypted = key.encrypt(text, 'base64')
+
+console.log('encrypted: ', encrypted)
+
+const decrypted = key.decrypt(encrypted, 'utf8')
+console.log('decrypted: ', decrypted)
